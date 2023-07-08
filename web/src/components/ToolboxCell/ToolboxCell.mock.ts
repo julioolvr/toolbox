05af54281c5963e41ec359318eda0238d6881b6d
Julio <julio.olvr@gmail.com>
2023-07-08 17:49:44 +0200

    Fix ToolboxCell test

--- web/src/components/ToolboxCell/ToolboxCell.mock.ts
+++ web/src/components/ToolboxCell/ToolboxCell.mock.ts
@@ -1,6 +1,14 @@
 // Define your own mock data here:
 export const standard = (/* vars, { ctx, req } */) => ({
   toolbox: {
+    // __typename: 'Toolbox',
     id: '42',
+    name: 'Toolbox',
+    description: 'Toolbox description',
+    tools: [],
+    user: {
+      username: 'Username',
+    },
+    userId: 'USER_ID',
   },
 })
