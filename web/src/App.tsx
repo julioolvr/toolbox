f8c407ce71e06af8c66090fcdada08f6823e7fa0
Julio <julio.olvr@gmail.com>
2023-07-08 21:12:36 +0200

    Dark mode support

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,4 +1,11 @@
-import { MantineProvider } from '@mantine/core'
+import { useEffect, useState } from 'react'
+
+import {
+  MantineProvider,
+  ColorSchemeProvider,
+  ColorScheme,
+} from '@mantine/core'
+import { useColorScheme } from '@mantine/hooks'
 
 import { FatalErrorBoundary, RedwoodProvider } from '@redwoodjs/web'
 import { RedwoodApolloProvider } from '@redwoodjs/web/apollo'
@@ -8,16 +15,38 @@ import Routes from 'src/Routes'
 
 import './index.css'
 
-const App = () => (
-  <FatalErrorBoundary page={FatalErrorPage}>
-    <RedwoodProvider titleTemplate="%PageTitle | %AppTitle">
-      <RedwoodApolloProvider>
-        <MantineProvider withGlobalStyles withNormalizeCSS>
-          <Routes />
-        </MantineProvider>
-      </RedwoodApolloProvider>
-    </RedwoodProvider>
-  </FatalErrorBoundary>
-)
+const App = () => {
+  const preferredColorScheme = useColorScheme()
+  const [colorScheme, setColorScheme] =
+    useState<ColorScheme>(preferredColorScheme)
+  const toggleColorScheme = (value?: ColorScheme) =>
+    setColorScheme(value || (colorScheme === 'dark' ? 'light' : 'dark'))
+
+  useEffect(
+    () => setColorScheme(preferredColorScheme),
+    [preferredColorScheme, setColorScheme]
+  )
+
+  return (
+    <FatalErrorBoundary page={FatalErrorPage}>
+      <RedwoodProvider titleTemplate="%PageTitle | %AppTitle">
+        <RedwoodApolloProvider>
+          <ColorSchemeProvider
+            colorScheme={colorScheme}
+            toggleColorScheme={toggleColorScheme}
+          >
+            <MantineProvider
+              withGlobalStyles
+              withNormalizeCSS
+              theme={{ colorScheme }}
+            >
+              <Routes />
+            </MantineProvider>
+          </ColorSchemeProvider>
+        </RedwoodApolloProvider>
+      </RedwoodProvider>
+    </FatalErrorBoundary>
+  )
+}
 
 export default App
